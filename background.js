da8d56550ec386c6e62d6c1836ea3cac9299b338
seojin7022 <60804781+seojin7022@users.noreply.github.com>
2022-01-01 16:47:47 +0900

    Add files via upload

--- background.js
+++ background.js
@@ -3,10 +3,10 @@ const button = document.querySelector("#button button");
 button.addEventListener("click", function () {
     
     const randNum1 = Math.floor(Math.random() * 16777215).toString(16);
-    const randNum2 = Math.floor(Math.random() * 16777215).toString(16);
+    let randNum2 = Math.floor(Math.random() * 16777215).toString(16);
     
     while (randNum1 === randNum2) {
-        const randNum2 = Math.floor(Math.random() * 16777215).toString(16);
+        randNum2 = Math.floor(Math.random() * 16777215).toString(16);
     }
     document.body.style.background = `linear-gradient(0deg, ${"#" + randNum1}, ${"#" + randNum2}`;
 });
--- clock.js
+++ clock.js
@@ -4,26 +4,26 @@ const title = body.querySelector("#title");
 
 
 function getTime() {
-    if (new Date().getMonth() === 1 && new Date().getDate() === 1) {
-        clock.classList.add("hidden");
-        title.innerHTML = "Happy New Year"
+    if (new Date().getMonth() === 0 && new Date().getDate() === 1) {
+        title.innerHTML = "오늘은 1월 1일!!";
+        clock.innerHTML = "새해 복 많이 받으세요";
     } else {
         const date = new Date();
-    let year = date.getFullYear();
+        let year = date.getFullYear();
 
-    if (date.getMonth() >= 1 && date.getDate() >= 1) {
-        year = year + 1;
-    }
+        if (date.getMonth() >= 0 && date.getDate() >= 1) {
+            year = year + 1;
+        }
 
 
-    const newYear = new Date(`${year} 1 1`);
-    let gap = newYear.getTime() - date.getTime();
-    const day = String(Math.floor(gap / (1000 * 60 * 60 * 24))).padStart(2, "0");
-    const hour = String(Math.floor(gap  % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))).padStart(2, "0");
-    const min = String(Math.floor(gap  % (1000 * 60 * 60) / (1000 * 60))).padStart(2, "0");
-    const sec = String(Math.floor(gap % (1000 * 60) / (1000))).padStart(2, "0");
-    
-    clock.innerHTML = `${day}일 ${hour}시간 ${min}분 ${sec}초`;
+        const newYear = new Date(`${year} 1 1`);
+        let gap = newYear.getTime() - date.getTime();
+        const day = String(Math.floor(gap / (1000 * 60 * 60 * 24))).padStart(2, "0");
+        const hour = String(Math.floor(gap  % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))).padStart(2, "0");
+        const min = String(Math.floor(gap  % (1000 * 60 * 60) / (1000 * 60))).padStart(2, "0");
+        const sec = String(Math.floor(gap % (1000 * 60) / (1000))).padStart(2, "0");
+
+        clock.innerHTML = `${day}일 ${hour}시간 ${min}분 ${sec}초`;
     }
     
 }
